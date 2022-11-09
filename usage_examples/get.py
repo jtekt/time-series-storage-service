43db5547a92cc2840e439fa6ed43fcf5de5251d2
Maxime Moreillon <m.moreillon@gmail.com>
2022-11-09 09:24:06 +0900

    example cleanup

--- usage_examples/get.py
+++ usage_examples/get.py
@@ -4,9 +4,6 @@ MEASUREMENT = 'dev3'
 
 url = f'http://localhost:7070/measurements/{MEASUREMENT}'
 
-# params = {'tags': ['tag1=v1', 'othertag2=v2']}
-#params = {'tags': ['tag1=v1']}
-# params = {'fields': ['temperature','humidity']}
 params = {'fields': 'temperature'}
 
 body = {'field': 'Temperature', 'value': 33.5}
